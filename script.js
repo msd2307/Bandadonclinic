c5bcdd7ebbabf7b821483191c76b6137195b2e2f
MSD <msd2307@gmail.com>
2026-02-02 02:13:00 +0300

    main1.0

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ async function send(){
     document.getElementById("msg").innerText="❌ Ошибка отправки";
   }
 }
+function scrollToForm() {
+  document.getElementById("form").scrollIntoView({ behavior: "smooth" });
+}
+
+// animation on scroll
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
